7ba3ae51bdb4220a42497c6a0d1d08a06e492e90
takuto terada <takuto.terada@rakus-partners.co.jp>
2022-08-29 12:07:27 +0900

    一覧ページ

---
+++ types/types.ts
@@ -0,0 +1,62 @@
+import { type } from "os";
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  zipcode: string;
+  address: string;
+  telephone: string;
+};
+
+export type Order = {
+  id: number;
+  userid: number;
+  totalPrice: number;
+  orderDate: Date;
+  distinationName: string;
+  distinationEmail: string;
+  distinationZipcode: string;
+  distinationAddress: string;
+  distinationTel: string;
+  deliveryTime: Date;
+  paymentMethod: number;
+  user: User;
+//   orderItemList: ;
+};
+
+export type OrderItem = {
+  id: number;
+  itemId: number;
+  orderId: number;
+  quantity: number;
+  size: string;
+  item: Item;
+//   orderToppingList: ;
+};
+
+export type Item = {
+  id: number;
+  type: string;
+  name: string;
+  discription: string;
+  price: number;
+  imagePath: string;
+  delete: boolean;
+//   toppingList: ?;
+};
+
+
+export type Topping={
+    id:number,
+    type:string,
+    name:string,
+}
+
+export type OrderTopping={
+    id:number,
+    toppingId:number,
+    orderItemId:number,
+    topping:Topping,
+}
