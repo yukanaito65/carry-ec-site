81dc6c44e67780659b1218512d9dd1588b7a6af7
Yuka Naito <yuka.naito@rakus-partners.co.jp>
2022-08-30 14:21:23 +0900

    ログイン画面作成途中

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/items',
+        destination: 'http://localhost:8000/items',
+      },
+      {
+        source: '/api/users',
+        destination: 'http://localhost:8000/users',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
--- pages/posts/login.tsx
+++ pages/posts/login.tsx
@@ -1,100 +1,138 @@
 import { useState } from 'react';
 import { User } from '../../types/types';
 import Link from 'next/link';
-
+import Head from 'next/head';
+import { Layout } from '../../component/layout';
+import styles from '../../styles/login.module.css';
 
 // idとメアドとパスワードをfetchで取得する
-export async function getAllJsonUser({
-  userData,
-}: {
-  userData: {
-    id: number;
-    email: string;
-    password: string;
-  };
-}) {
-  return fetch(`http://localhost:3000/items/${userData.id}`)
+export async function getAllJsonUser() {
+  return fetch(`http://localhost:8000/users/`)
     .then((res) => res.json())
     .then((data) => {
       return data.map((data: User) => {
         return {
-          params: {
+          user: {
             id: data.id,
             email: data.email,
             password: data.password,
+            logined: data.logined,
           },
         };
       });
-    })
+    });
 }
-
-
-// ログインボタンをクリックした時に、Userのloginedをtrueにする or エラー表示する
-function onCkickMatch() {
-    if (
-        data.email === email.target.value &&
-        data.password === password.target.value
-      ) { // Userのloginedをtrueにする
-      } else {
-        // <p>メールアドレス、またはパスワードが間違っています</p>を表示する
-      }
-}
-
 
 // html
 export default function Login() {
   const initialValue = { mail: '', pass: '' };
-  const [formValue, setFormValue] = useState(initialValue);
+  const [formValue, setFormValue] = useState<any>(initialValue);
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
-    setFormValue({ ...formValue, [name]: value });
+    let newFormValue = formValue;
+    newFormValue[name] = value;
+    setFormValue(newFormValue);
     console.log(formValue);
   };
 
+  // ログインボタンをクリックした時に、Userのloginedをtrueにする & ページ遷移する”
+  async function onCkickMatch() {
+    const users = await getAllJsonUser();
+
+    // console.log(users[0]);
+    users.forEach(
+      (user: {
+        id: number;
+        email: string;
+        password: string;
+        logined: boolean;
+      }) => {
+        if (
+          user.email === formValue.mail &&
+          user.password === formValue.pass
+        ) {
+          user.logined = true;
+        }
+      }
+    );
+    return fetch('http://localhost:8000/users', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(users),
+    }).then((res) => res.json());
+  }
+
   return (
     <>
-      <form>
-        <h1>ログイン</h1>
-        <hr />
-        <div>
-
+      <Head>
+        <title>ログイン画面</title>
+      </Head>
+      <Layout>
+        <form className={styles.formContainer}>
+          <h1 className={styles.h1}>ログイン</h1>
           <div>
-            <label>メールアドレス：</label>
-            <input
-              type="email"
-              placeholder="Email"
-              name="mail"
-              id="email"
-              onChange={(e) => handleChange(e)}
-            />
+            <div>
+              <div className={styles.labelError}>
+                <label className={styles.label}>
+                  メールアドレス：
+                </label>
+                {formValue.mail === '' && (
+                  <p className={styles.error}>
+                    メールアドレスを入力してください
+                  </p>
+                )}
+                <br />
+              </div>
+              <input
+                className={styles.input}
+                type="email"
+                placeholder="Email"
+                name="mail"
+                id="email"
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+
+            <div>
+              <div className={styles.labelError}>
+                <label className={styles.label}>パスワード：</label>
+                {formValue.pass === '' && (
+                  <p className={styles.error}>
+                    パスワードを入力してください
+                  </p>
+                )}
+                <br />
+              </div>
+              <input
+                className={styles.input}
+                type="password"
+                placeholder="Password"
+                name="pass"
+                id="password"
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+            {formValue.pass === '' && (
+              <button onClick={() => onCkickMatch()}>ログイン</button>
+            )}
+            {!(formValue.pass === '') && (
+              <Link href="/">
+                <button className={styles.loginBtn} onClick={() => onCkickMatch()}>
+                  ログイン
+                </button>
+              </Link>
+            )}
           </div>
+        </form>
 
-          <div>
-            <label>パスワード：</label>
-            <input
-              type="password"
-              placeholder="Password"
-              name="pass"
-              id="password"
-              onChange={(e) => handleChange(e)}
-            />
-          </div>
-
-          <Link href="/">
-          {/* クリックイベント2つ書きたい */}
-          <button ocClick={() => onCkickMatch() &&
-            // ログイン後の一覧表示画面へ遷移
-          }>ログイン</button>
-          </Link>
-
-        </div>
-      </form>
-
-      <Link href="../create">
-        <a>ユーザー登録はこちら</a>
-      </ Link>
-
+        <Link href="../create">
+          <a className={styles.forCreate}>ユーザー登録はこちら</a>
+        </Link>
+      </Layout>
     </>
   );
 }
+
+// エラーメッセが消えない -> 17業目のエラーが関係してる？
+// db.jsonに存在してるアドレスとパスを入れてもページ遷移されない -> 不明
